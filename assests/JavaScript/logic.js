15eb0f86db4874d23451b9ab92c1376e737d905c
hegratrix <gratrixmomma@yahoo.com>
2018-08-30 08:53:19 -0700

    96% done

--- assests/JavaScript/logic.js
+++ assests/JavaScript/logic.js
@@ -1,30 +1,219 @@
-// opens curtains
-$(document).ready(function() {
-    $('#curtain1').animate({width: '200px'}, 3000)
-    $('#curtain2').animate({width: '200px'}, 3000)
-})
-
-
-//randomly pick 10 movies for quiz
+let correct = 0
+let incorrect = 0
+let unanswered = 0
+let index = 0
 let randomMovieArray = []
 let moviePool = movieDataBase.length
-for (i = 0; i < 10; i++) {
-    let movieNumber = Math.floor(Math.random()*(moviePool))
-    if (randomMovieArray.indexOf(movieNumber) === -1) {
-    randomMovieArray.push(movieNumber)
-    console.log(randomMovieArray)
-    } else {
-        i--
+let status = true
+let m = -1
+let timer = setInterval
+
+//which game is selected
+let newGame = decodeURIComponent(window.location.search);
+newGame = newGame.substring(1);
+finalGame = newGame.replace('para1=','');
+if (finalGame === "what") {
+    $('#curtain1').animate({width: '260px'}, 2000)
+    $('#curtain2').animate({width: '260px'}, 2000)
+    gameSetUp()
+    setTimeout (playWhatMovie, 2500)
+} else {
+    $('#curtain1').animate({width: '260px'}, 2000)
+    $('#curtain2').animate({width: '260px'}, 2000)
+    gameSetUp()
+    setTimeout (playWhoSaid, 2500)
+}
+
+function gameSetUp () {
+    //randomly pick 10 movies for quiz
+    for (i = 0; i < 10; i++) {
+        let movieNumber = Math.floor(Math.random()*(moviePool))
+        if (randomMovieArray.indexOf(movieNumber) === -1) {
+            randomMovieArray.push(movieNumber)
+        } else {
+            i--
+        }
+    }
+    //get info from 10 random movies
+    for (i = 0; i < randomMovieArray.length; i++) {
+        let index = randomMovieArray[i]
     }
 }
 
-//get info from 10 random movies
-for (i = 0; i < randomMovieArray.length; i++) {
-    let index = randomMovieArray[i]
-    console.log(index)
-    $('#movie').append(`
-    <h1>${index}  ${movieDataBase[index].title}</h1>
-    <img src = "${movieDataBase[index].picture}">
-    <h3>${movieDataBase[index].speaker}</h3>
+//plays what movie
+function playWhatMovie () { 
+    if (index < randomMovieArray.length) {
+        m = randomMovieArray[index]
+        $('.movie-answer').html(`
+            <div class = "question">
+            <h2 id = "in-which-movie">In which movie is it said:</h2>
+            <h2 id ="question-movie-line">${m}"${movieDataBase[m].line}"</h2>
+            <div class="choice-container"></div>  
+            </div>
+            `)
+        whatMovieOptions()
+        setTimer (playWhatMovie) 
+    } else {
+        endGame()
+    }
+}
+
+//plays who said
+function playWhoSaid (){
+    if (index < randomMovieArray.length) {
+        m = randomMovieArray[index]
+        $('.movie-answer').html(`
+            <div class = "question">
+            <h2 id = "in-which-movie">Who said:</h2>
+            <h2 id ="question-movie-line">"${movieDataBase[m].line}"</h2>
+            <div class="choice-container"></div> 
+            </div>
+            `)
+        whoSaidOptions()
+        setTimer (playWhoSaid) 
+    } else {
+        endGame()
+    }
+}
+
+//sets timer
+function setTimer (game) {
+    let t = 10
+    timer = setInterval (function () {
+        $("#timer").html(t + " seconds")
+        t--
+        if (t < 0) {
+            unanswered++
+            clearInterval(timer)
+            showAnswer('', game)
+        }
+    }, 1000)
+}
+
+//create random choice options
+function whatMovieOptions () {
+    let choiceOptions = []
+    choiceOptions.push(m)
+    for (i=0; i < 3; i++) {
+        let choice = Math.floor(Math.random()*(moviePool))
+        if (choice !== m) {
+            choiceOptions.push(choice)
+        } else {
+            i--
+        }
+    }
+    shuffle(choiceOptions)
+    for (i = 0; i < 4; i++) {
+        let y = choiceOptions[i]
+        if (y === m) {
+            $('.choice-container').append (`
+                <input type="checkbox" class = "choice-boxes" onclick= "checkAnswer(true, playWhatMovie)">${movieDataBase[y].title}<br>
+                `)
+        } else {
+            $('.choice-container').append (`
+                <input type="checkbox" class = "choice-boxes" onclick= "checkAnswer(false, playWhatMovie)">${movieDataBase[y].title}<br>
+                `)
+        }
+    }
+}              
+
+//create random choice options
+function whoSaidOptions () {
+    let choiceOptions = []
+    choiceOptions.push(m)
+    for (i=0; i < 3; i++) {
+        let choice = Math.floor(Math.random()*(moviePool))
+        if (choice !== m) {
+            choiceOptions.push(choice)
+        } else {
+            i--
+        }
+    }
+    shuffle(choiceOptions)
+    for (i = 0; i < 4; i++) {
+        let y = choiceOptions[i]
+        if (y === m) {
+            $('.choice-container').append (`
+                <input type="checkbox" class = "choice-boxes" value ="true" onclick= "checkAnswer(true, playWhoSaid)">${movieDataBase[y].speaker}<br>
+                `)
+        } else {
+            $('.choice-container').append (`
+                <input type="checkbox" class = "choice-boxes" value ="false" onclick= "checkAnswer(false, playWhoSaid)">${movieDataBase[y].speaker}<br>
+                `)
+        }
+    }
+}  
+
+//shuffles array so choices in random order
+function shuffle (array) {
+    let i = 0
+    let j = 0
+    let temp = []
+  for (i = array.length - 1; i > 0; i -= 1) {
+    j = Math.floor(Math.random() * (i + 1))
+    temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return temp
+}
+
+function checkAnswer(value, game){
+    clearInterval(timer)
+    if (value === true) {
+        correct++
+    } else  {
+        incorrect++
+    }
+    showAnswer(value, game)
+}
+
+function showAnswer (value, game) {
+    let status = ''
+    if (value === true) {
+        status = 'Awesome, you are correct!'
+    } else {
+        status = 'Bummer, you are wrong!'
+    }
+    $('#timer').html('')
+    $('.movie-answer').html(`
+        <div class = "${m} movie-reveal">
+        <h2 class = "answer-status">${status}</h2>
+        <h2 class ="${m} movie-line">"${movieDataBase[m].line}"</h2>
+        <h3 class = "${m} movie-speaker">${movieDataBase[m].speaker}</h3>
+        <h1 class = "${m} movie-title">${movieDataBase[m].title}</h1>
+        <h3 class = "${m} movie-year">(${movieDataBase[m].year})</h3>
+        </div>
+        <img class="${m} poster" src = "${movieDataBase[m].picture}">
+        `)
+    index++
+    console.log(game)
+    setTimeout(game, 3000)
+}
+
+function endGame () {
+    $('.movie-answer').html(`
+        <div id = "end-game">
+            <h1 id ="game-over">Game Over</h1>
+            <h2>Correct: ${correct}</h2>
+            <h2>Incorrect: ${incorrect}</h2>
+            <h2>Unanswered: ${unanswered}</h2>
+            <h1 id ="again">Want to play again?</h1>
+            <div id ="buttons">
+                <button id = "what-movie2" class ="index-pg" onclick= "reset(playWhatMovie)">What Movie?</button>
+                <button id = "who-said2" class ="index-pg" onclick= "reset(playWhoSaid)">Who Said it?</button>
+            </div>
+        </div>
     `)
+}
+
+function reset (game) {
+    correct = 0
+    incorrect = 0
+    unanswered = 0
+    index = 0
+    randomMovieArray = []
+    gameSetUp()
+    console.log(game)
+    game()
 }
--- assests/JavaScript/movies.js
+++ assests/JavaScript/movies.js
@@ -2,259 +2,302 @@ let movieDataBase= [
     {"title": "Love Story",
         "line": "Love means never having to say you're sorry.",
         "speaker": "Ryan O'Neal",
-        "picture": "assests/images/love_story.jpg"
+        "picture": "assests/images/love_story.jpg",
+        "year": "1970"
     },
 
     {"title": "Gone With the Wind",
         "line": "Frankly, my dear, I don't give a damn.",
         "speaker": "Clark Gable",
-        "picture": "assests/images/gone_wind.jpg"
+        "picture": "assests/images/gone_wind.jpg",
+        "year": "1939"
     },
 
     {"title": "The Godfather",
         "line": "I'm going to make him an offer he can't refuse.",
         "speaker": "Marlon Brando",
-        "picture": "assests/images/godfather.jpg"
+        "picture": "assests/images/godfather.jpg",
+        "year": "1972"
     },
 
     {"title": "Casablanca",
         "line": "Here's looking at you, kid.",
         "speaker": "Humphrey Bogart",
-        "picture": "assests/images/Casablanca.jpg"
+        "picture": "assests/images/Casablanca.jpg",
+        "year": "1942"
     },
 
     {"title": "Sudden Impact",
         "line": "Go ahead, make my day",
         "speaker": "Clint Eastwood",
-        "picture": "assests/images/sudden_impact.jpg"
+        "picture": "assests/images/sudden_impact.jpg",
+        "year": "1983"
     },
 
     {"title": "All About Eve",
         "line": "Fasten your seatbelts, it's going to be a bumpy night.",
         "speaker": "Bette Davis",
-        "picture": "assests/images/Eve.jpg"
+        "picture": "assests/images/Eve.jpg",
+        "year": "1950"
     },
 
     {"title": "Taxi Driver",
         "line": "You talkin' to me?",
         "speaker": "Robert DeNiro",
-        "picture": "assests/images/taxi_driver.jpg"
+        "picture": "assests/images/taxi_driver.jpg",
+        "year": "1976"
     },
 
     {"title": "Cool Hand Luke",
         "line": "What we've got here is a failure to communicate",
         "speaker": "Strother Martin",
-        "picture": "assests/images/Luke.jpg"
+        "picture": "assests/images/Luke.jpg",
+        "year": "1967"
     },
 
     {"title": "Apocalypse Now",
-        "line": "I love the smell of napalm in the morning",
+        "line": "I love the smell of napalm in the morning.",
         "speaker": "Robert Duvall",
-        "picture": "assests/images/apocalypse.jpg"
+        "picture": "assests/images/apocalypse.jpg",
+        "year": "1979"
     },
 
     {"title": "The Maltese Falcon",
         "line": "The stuff that dreams are made of.",
         "speaker": "Humphrey Bogart",
-        "picture": "assests/images/maltese.jpg"
+        "picture": "assests/images/maltese.jpg",
+        "year": "1941"
     },
 
     {"title": "In the Heat of the Night",
         "line": "They call me Mister Tibbs!",
         "speaker": "Sidney Poitier",
-        "picture": "assests/images/heat_night.jpg"
+        "picture": "assests/images/heat_night.jpg",
+        "year": "1967"
     },
 
     {"title": "White Heat",
         "line": "Made it, Ma! Top of the world!",
         "speaker": "James Cagney",
-        "picture": "assests/images/white_heat.jpg"
+        "picture": "assests/images/white_heat.jpg",
+        "year": "1949"
     },
 
     {"title": "Network",
-        "line": "I'm mad as hell, and I'm not gonig ot take this anymore!",
+        "line": "I'm mad as hell, and I'm not going to take this anymore!",
         "speaker": "Peter Finch",
-        "picture": "assests/images/network.jpg"
+        "picture": "assests/images/network.jpg",
+        "year": "1976"
     },
 
     {"title": "Casablanca",
-        "line": "Louis, I think this is the beginning of a beautfil friendship.",
+        "line": "Louis, I think this is the beginning of a beautiful friendship.",
         "speaker": "Humphrey Bogart",
-        "picture": "assests/images/Casablanca.jpg"
+        "picture": "assests/images/Casablanca.jpg",
+        "year": "1942"
     },
 
     {"title": "Wizard of Oz",
         "line": "There's no place like home.",
         "speaker": "Judy Garland",
-        "picture": "assests/images/wizard.jpg"
+        "picture": "assests/images/wizard.jpg",
+        "year": "1939"
     },
 
     {"title": "Jerry Maguire",
         "line": "Show me the money!",
         "speaker": "Cuba Gooding, Jr",
-        "picture": "assests/images/Maguire.jpg"
+        "picture": "assests/images/Maguire.jpg",
+        "year": "1996"
     },
 
     {"title": "A Few Good Men",
         "line": "You can't handle the truth!",
         "speaker": "Jack Nicholson",
-        "picture": "assests/images/few_good_men.jpg"
+        "picture": "assests/images/few_good_men.jpg",
+        "year": "1992"
     },
 
     {"title": "Gone With the Wind",
         "line": "After all, tomorrow is another day.",
         "speaker": "Vivien Leigh",
-        "picture": "assests/images/gone_wind.jpg"
+        "picture": "assests/images/gone_wind.jpg",
+        "year": "1939"
     },
 
     {"title": "When Harry Met Sally",
         "line": "I'll have what she's having.",
         "speaker": "Estelle Reiner",
-        "picture": "assests/images/harry_sally.jpg"
+        "picture": "assests/images/harry_sally.jpg",
+        "year": "1989"
     },
 
     {"title": "Jaws",
         "line": "You're gonna need a bigger boat.",
         "speaker": "Roy Scheider",
-        "picture": "assests/images/jaws.jpg"
+        "picture": "assests/images/jaws.jpg",
+        "year": "1975"
     },
 
     {"title": "The Terminator",
         "line": "I'll be back",
         "speaker": "Arnold Schwarzenegger",
-        "picture": "assests/images/terminator.jpg"
+        "picture": "assests/images/terminator.jpg",
+        "year": "1984"
     },
 
     {"title": "The Pride of the Yankees",
-        "line": "Today, I consider myyself the luckiest man on the face of the earth.",
+        "line": "Today, I consider myself the luckiest man on the face of the earth.",
         "speaker": "Gary Cooper",
-        "picture": "assests/images/yankees.jpg"
+        "picture": "assests/images/yankees.jpg",
+        "year": "1942"
     },
 
     {"title": "Field of Dreams",
         "line": "If you build it, they will come.",
         "speaker": "Ray Liotta",
-        "picture": "assests/images/field_dreams.jpg"
+        "picture": "assests/images/field_dreams.jpg",
+        "year": "1989"
     },
 
     {"title": "Forrest Gump",
-        "line": "My mama always said, 'Life is like a box of chocolates.  You nevear know what you're gonna get.",
+        "line": "My mama always said, 'Life is like a box of chocolates.  You never know what you're gonna get.'",
         "speaker": "Tom Hanks",
-        "picture": "assests/images/forrest_gump.jpg"
+        "picture": "assests/images/forrest_gump.jpg",
+        "year": "1994"
     },
 
     {"title": "The Sixth Sense",
         "line": "I see dead people.",
         "speaker": "Haley Joel Osment",
-        "picture": "assests/images/sixth_sense.jpg"
+        "picture": "assests/images/sixth_sense.jpg",
+        "year": "1999"
     },
 
     {"title": "A Streetcar Named Desire",
         "line": "Stella! Hey, Stella!",
         "speaker": "Marlon Brando",
-        "picture": "assests/images/streetcar.jpg"
+        "picture": "assests/images/streetcar.jpg",
+        "year": "1951"
     },
 
     {"title": "Frankenstein",
         "line": "It's alive! It's alive!",
         "speaker": "Colin Clive",
-        "picture": "assests/images/frankenstein.jpg"
+        "picture": "assests/images/frankenstein.jpg",
+        "year": "1931"
     },
 
     {"title": "Apollo 13",
         "line": "Houston, we have a problem.",
         "speaker": "Tom Hanks",
-        "picture": "assests/images/apollo_13.jpg"
+        "picture": "assests/images/apollo_13.jpg",
+        "year": "1995"
     },
 
     {"title": "Dirty Harry",
-        "line": "You've got to ask yourself one question: 'Do I feel lucky? Well, do ya, punk?",
+        "line": "You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk?",
         "speaker": "Clint Eastwood",
-        "picture": "assests/images/dirty_harry.jpg"
+        "picture": "assests/images/dirty_harry.jpg",
+        "year": "1971"
     },
 
     {"title": "Jerry Maguire",
         "line": "You had me at hello.",
         "speaker": "Renee Zellweger",
-        "picture": "assests/images/maguire.jpg"
+        "picture": "assests/images/maguire.jpg",
+        "year": "1996"
     },
 
     {"title": "A League of Their Own",
         "line": "There's no crying in baseball.",
         "speaker": "Tom Hanks",
-        "picture": "assests/images/league_own.jpg"
+        "picture": "assests/images/league_own.jpg",
+        "year": "1992"
     },
 
     {"title": "Psycho",
         "line": "A boy's best friend is his mother.",
         "speaker": "Anthony Perkins",
-        "picture": "assests/images/psycho.jpg"
+        "picture": "assests/images/psycho.jpg",
+        "year": "1960"
     },
 
     {"title": "The Godfather: Part II",
         "line": "Keep your friends close, but your enemies closer.",
         "speaker": "Al Pacino",
-        "picture": "assests/images/godfather.jpg"
+        "picture": "assests/images/godfather.jpg",
+        "year": "1974"
     },
 
     {"title": "Sons of the Desert",
         "line": "Well, here's another nice mess you've gotten me into!",
         "speaker": "Oliver Hardy",
-        "picture": "assests/images/godfather2.jpg"
+        "picture": "assests/images/godfather2.jpg",
+        "year": "1933"
     },
 
     {"title": "Scarface",
         "line": "Say 'hello' to my little friend.",
         "speaker": "Al Pacino",
-        "picture": "assests/images/scarface.jpg"
+        "picture": "assests/images/scarface.jpg",
+        "year": "1983"
     },
 
     {"title": "The Shining",
         "line": "Here's Johnny!",
         "speaker": "Jack Nicholson",
-        "picture": "assests/images/shining.jpg"
+        "picture": "assests/images/shining.jpg",
+        "year": "1980"
     },
 
     {"title": "Poltergeist",
         "line": "They're here!",
         "speaker": "Heather O'Rourke",
-        "picture": "assests/images/poltergeist.jpg"
+        "picture": "assests/images/poltergeist.jpg",
+        "year": "1982"
     },
 
     {"title": "Rocky",
         "line": "Yo, Adrian",
         "speaker": "Sylvester Stallone",
-        "picture": "assests/images/rocky.jpg"
+        "picture": "assests/images/rocky.jpg",
+        "year": "1976"
     },
 
     {"title": "Funny Girl",
         "line": "Hello, gorgeous.",
         "speaker": "Barbara Streisand",
-        "picture": "assests/images/funny_girl.jpg"
+        "picture": "assests/images/funny_girl.jpg",
+        "year": "1968"
     },
 
     {"title": "The Lord of the Rings: The Two Towers",
-        "line": "My precious",
+        "line": "My precious!",
         "speaker": "Andy Serkis",
-        "picture": "assests/images/lord_rings2.jpg"
+        "picture": "assests/images/lord_rings2.jpg",
+        "year": "2002"
     },
 
     {"title": "Moonstruck",
         "line": "Snap out of it!",
         "speaker": "Cher",
-        "picture": "assests/images/moonstruck.jpg"
+        "picture": "assests/images/moonstruck.jpg",
+        "year": "1987"
     },
 
     {"title": "Dirty Dancing",
         "line": "Nobody puts Baby in the corner.",
         "speaker": "Patrick Swayze",
-        "picture": "assests/images/dirty_dancing.jpg"
+        "picture": "assests/images/dirty_dancing.jpg",
+        "year": "1987"
     },
 
     {"title": "Titanic",
-        "line": "I'm kind of the world!",
+        "line": "I'm king of the world!",
         "speaker": "Leonardo DiCaprio",
-        "picture": "assests/images/titanic.jpg"
+        "picture": "assests/images/titanic.jpg",
+        "year": "1997"
     },
     
 ]
